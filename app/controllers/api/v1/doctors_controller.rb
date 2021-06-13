c08e2c64c39b1478f993143f3a0fcfb90153deb6
Juan José Ramírez <jjramirez@pucp.pe>
2021-06-13 11:41:31 -0500

    Add doctors CRUD

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,7 +1,40 @@
 module Api::V1
   class DoctorsController < ApiController
-    def index
-      render json: Doctor.all
+    before_action :set_doctor, only: [:show, :update, :destroy]
+
+    def show
+      render json: DoctorSerializer.new(@doctor).serializable_hash
     end
+
+    def index
+      doctors = Doctor.all
+      render json: DoctorSerializer.new(doctors).serializable_hash
+    end
+
+    def create
+      doctor = Doctor.create!(doctor_params)
+      render json: DoctorSerializer.new(doctor).serializable_hash, status: :created
+    end
+
+    def update
+      @doctor.update!(doctor_params)
+      render json: DoctorSerializer.new(@doctor).serializable_hash, status: :created
+    end
+
+    def destroy
+      @doctor.destroy!
+      render json: DoctorSerializer.new(@doctor).serializable_hash
+    end
+
+    private
+
+    def doctor_params
+      params.require(:doctor).permit(:name)
+    end
+
+    def set_doctor
+      @doctor = Doctor.find(params[:id])
+    end
+
   end
 end
---
+++ app/serializers/doctor_serializer.rb
@@ -0,0 +1,5 @@
+class DoctorSerializer
+  include JSONAPI::Serializer
+
+  attributes :name
+end
